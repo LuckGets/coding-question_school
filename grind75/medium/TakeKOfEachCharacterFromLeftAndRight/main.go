e0f1ac07975a250d0771d0ab429c5ce7763382fb
LuckGets <kasdies12@gmail.com>
2024-11-20 13:15:46 +0700

    Add:gring75/medium/TakeKOfEachCharFromLeftAndRight__GO

---
+++ grind75/medium/TakeKOfEachCharacterFromLeftAndRight/main.go
@@ -0,0 +1,70 @@
+package takeCharacters
+
+/**
+*  This is actually hard sliding window question for me in term of intuition.
+*  You can actually using two pointer combining with hashSet that have key as
+*  a,b and c and iterate through array to find the pre and suf.
+*  but we will invert thinking for this question.
+*  the result is finding the minimum number of minutes to take each
+*  char from left and right. So, the invert is the maximum char we can
+*  remove from the string to have the required amount of char in the result string
+*  -------------------------------------------
+*  ## Than how to?
+*  The intuition is we will find the maximum window which can remove
+*  and the outer window still have the amount of char same as the required amount
+*  So, we will find the count of all character for two reason.
+*  One, to check does the amount of char in string match the required amount or not, if not,
+*  we will return -1
+*  Two, to have the references for the sliding window, by using sliding window,
+*  we will using two pointer to keep track of window. We will increment the right pointer while we
+*  iterate through string and minus the count of character from the outside window.
+*  then, we will check if the outside window have the amount of char lower than the required amount or no,
+*  if yes, than we will move the left pointer untill it match the right pointer or the amount of char in
+*  outside window is equal to required amount
+*	 once, the window is valid which mean the outside window is still contain all char with required amount
+*	 we will update the maximum window.
+*	 after finish the iteration, we will have the maximum window of the string that can remove and
+*	 the outside window is still valid.
+*	 So, the result of this question will be the length of string minus with maximum window
+ */
+
+func takeCharacters(s string, k int) int {
+	// we declare the array to collect the number of each char
+	countAllChar := []int{0, 0, 0}
+	n := len(s)
+	// we count how many amount of char in the string
+	for c := range s {
+		ind := int(s[c]) - int('a')
+		countAllChar[ind]++
+	}
+
+	// if the string is invalid, we will return -1
+	if countAllChar[0] < k || countAllChar[1] < k || countAllChar[2] < k {
+		return -1
+	}
+
+	// we declare left pointer and maximum window
+	l, maxWindow := 0, 0
+	// Iterate through each char
+	for r := range s {
+		// we will compare the index of countArray by find the ascii value
+		ind := int(s[r]) - int('a')
+		// subtract the count of the char
+		countAllChar[ind]--
+
+		// than we will check if current window is valid or no, by compare each count of char to
+		// the required amount.
+		// if window is not valid, we will shrink the window by moving the left pointer
+		// to right pointer and return the char amount to the Counting array.
+		for l <= r && (countAllChar[0] < k || countAllChar[1] < k || countAllChar[2] < k) {
+			lInd := int(s[l]) - int('a')
+			countAllChar[lInd]++
+			l++
+		}
+
+		// Update the maximumWindow by compare the old maxwindow to current window
+		maxWindow = max(maxWindow, (r - l + 1))
+	}
+	// The result is the subtract of length of string and maximum window
+	return n - maxWindow
+}
